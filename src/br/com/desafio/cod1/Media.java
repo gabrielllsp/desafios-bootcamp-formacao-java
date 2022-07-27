ac4dbfc8983bf6a4fab0155f9f3cbbfedbe8a7dc
Gabrielllsp <gabriel.amoreira@outlook.com>
2022-07-27 11:39:19 -0300

    first commit

---
+++ src/br/com/desafio/cod1/Media.java
@@ -0,0 +1,20 @@
+package br.com.desafio.cod1;
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+
+        //TODO: Complete os espaços em branco com uma possível solução para o desafio
+        media = (A * 3.5 +  B * 7.5)/11;
+
+        System.out.printf("MEDIA = %.5f", media);
+        System.out.println();
+    }
+}
